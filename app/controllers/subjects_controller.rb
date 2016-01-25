cc4b72e8f8dbd38045975e697bb5728571d0821b
onyoo <roberto9m9@gmail.com>
2016-01-25 22:37:26 +0100

    object index links at application partial level

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,22 +1,41 @@
 class SubjectsController < ApplicationController
   def new
+    @subject = Subject.new
   end
 
   def create
+    @subject = Subject.find_or_create_by(subject_params)
+    redirect_to subject_path(@subject)
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id])
+
+    @subject.update(subject_params)
+
+    redirect_to subject_path(@subject)
   end
 
   def destroy
+    binding.pry
+    Subject.destroy(params[:id])
   end
 
   def index
+    @subjects = Subject.all
   end
 
   def show
+    @subjects = Subject.all
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name)
   end
 end
