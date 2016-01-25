e70c7270d7659bc0e912317df6b39304a5295329
onyoo <roberto9m9@gmail.com>
2016-01-25 19:41:27 +0100

    home route drawn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :users
   resources :subjects
   resources :categories
-  resources :recources
+  resources :resources
+
+  get 'home' => 'application#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
