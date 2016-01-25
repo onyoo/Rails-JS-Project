3ce5e76e6a42fa3e97d93f8642394372db35bb82
onyoo <roberto9m9@gmail.com>
2016-01-25 21:52:50 +0100

    category controller actions new, create, edit, and update created, and new category partial

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,27 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def create
+    redirect to category_path(Category.create(category_params))
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    @category.update(category_params)
+
+    redirect_to category_path(@category)
   end
 
   def destroy
+    binding.pry
+    Category.destroy(params[:id])
   end
 
   def index
@@ -19,4 +29,11 @@ class CategoriesController < ApplicationController
 
   def show
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
