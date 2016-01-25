c4248e6b76ba517d48461b5fb6683bb7f36bf5cc
onyoo <roberto9m9@gmail.com>
2016-01-25 14:12:28 +0100

    model associations added subject,topic,category,resource,user

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,7 @@
 class Subject < ActiveRecord::Base
+  belongs_to :user
+  has_many :resources
+  has_many :subjects
+  has_many :categories, through: :subjects
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :subjects
+  has_many :categories, through: :subjects
+  has_many :resources
 end
