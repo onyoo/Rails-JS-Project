c43b898233aee04885ba67c22d73f17e899d7b3f
onyoo <roberto9m9@gmail.com>
2016-01-26 22:17:19 +0100

    welcome message set, header login/out closure changed to #current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user   ## allows method to be called in views, not just other controllers
+  helper_method :current_user , :require_user  ## allows method to be called in views, not just other controllers
 
   def home
     render "home"
